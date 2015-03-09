4a359333d7255b69288d7b1ba2cf7c64d09fcc98
Panu Kouri <panu.kouri@luonadesign.com>
2015-03-09 12:47:24 +0200

    Initial version: insert and view relation fields.

--- client/data/joins/joins.js
+++
@@ -1,8 +0,0 @@
-//////////////////////////// ELEMENT: JOINS ////////////////////////////
-/*
-- An abstract class for handling mongo database joins.
-*/
-Societies2Users = new Mongo.Collection("societies2users");
-Meteor.subscribe("societies2users");
-
-//////////////////////////// END OF FILE ////////////////////////////
