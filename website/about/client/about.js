7f0e8efb1804714431b25a17b82a992fb7171059
Panu Kouri <panu.kouri@luonadesign.com>
2015-03-26 11:40:56 +0200

    More contents and better graphics for About page.

--- website/about/client/about.js
+++ website/about/client/about.js
@@ -2,10 +2,28 @@ Template.skitAbout.rendered = function () {
   $('.skitAbout .ui.accordion').accordion();
 }
 
-skitSession.setDefault("skitAbout", "skitAboutEasyWayToChange");
+skitSession.setDefault("skitAbout", "skitAboutFront");
 
 Template.skitAbout.helpers({
   aboutPage: function () {
     return Session.get("skitAbout");
+  }
+});
+
+Template.skitAboutNext.helpers({
+  nextPageTemplate: function () {
+    switch(Session.get("skitAbout")) {
+      case "skitAboutFront": return "skitAboutCitizen";
+      case "skitAboutCitizen": return "skitAboutCompany";
+      default: return "skitAboutFront";
+    }
+  },
+  
+  nextPageTitle: function () {
+    switch(Session.get("skitAbout")) {
+      case "skitAboutFront": return "1.1 Citizen";
+      case "skitAboutCitizen": return "1.2 Company";
+      default: return "1. Society Kit";
+    }
   }
 });
